7041e15d79bb15d5e87c495859cc7898aa0d14c3
g-solomei <luigi.bardella@solomei.ai>
2025-09-01 17:53:51 +0200

    Integrate AletheiaUI component with functional tabs and profile fetching logic. Add initial CSS for layout and styling.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AletheiaUI from "./components/AletheiaUI/aletheiaUI.tsx";
+
+function App() {
+
+  return (
+    <>
+     <AletheiaUI/>
+    </>
+  )
+}
+
+export default App
